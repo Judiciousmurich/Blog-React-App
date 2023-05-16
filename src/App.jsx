093e1feed1d4f0070b9ecfaa636959f78ca39fa1
Judith Muthui <122685872+Judiciousmurich@users.noreply.github.com>
2023-05-16 21:03:16 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,14 @@ import './App.css';
 import Blog from './blog';
 import Hightlights from './sidebar';
 
+
+
 function App() {
+   const date = new Date();
+  const day = date.getDay()
+  const year = date.getFullYear()
+  const month = date.getMonth()
+  const myDate = `${year} / ${month} / ${day}`;
   return (
      <div className="app-container">
       <div className="header">
@@ -11,8 +18,15 @@ function App() {
       <div className="flex-container">
         <Blog />
         <Hightlights />
+    <footer>
+          <span className="myname">JudithMuthui@theJitu</span>
+          <span className="myname">
+            {myDate}
+          </span>
+        </footer>
       </div>
      </div>
+    
   );
 }
 
